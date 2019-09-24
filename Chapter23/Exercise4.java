b9a37f0e1a9ab662101b775f3ad5b262d194dc3e
Chris Urbano <ChrisUrbano1116@gmail.com>
2019-09-23 21:18:16 -0600

    Updating chapter 23

---
+++ Chapter23/Exercise4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Exercise4
+{
+    public static void main( String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
+        double x = 0;
+        int n = 1;
+        double term = 1;
+        double sum = 1;
+
+        System.out.println("Enter the x value: ");
+        x = scan.nextDouble();
+        scan.close();
+
+        while(term > 1.0E-12)
+        {
+            term *= x/n;
+            sum += term;
+            n++;
+            System.out.println("n:" + n +  "\tterm: " + term + "\tsum: " + sum);
+        }            
+        System.out.println("My   e^x: " + sum);
+        System.out.println("Real e^x: " + Math.exp(x));
+    }
+}
